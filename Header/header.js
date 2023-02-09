51faa13f9ca469a6d5f4a9ae187faab41b932601
MatthewDlr <matthieu.delarue5@gmail.com>
2023-02-09 14:57:52 +0100

    Header update

---
+++ Header/header.js
@@ -0,0 +1,23 @@
+/* This file contains the header, modify the html code below to modify the header */
+
+document.write(`
+
+    <link rel="stylesheet" href="Header/header.css"/>
+    <div class="header">
+        <div class="header-left">
+        <img
+            onclick="window.location.href='index.html'"
+            src="images/Logo.svg"
+            alt="HeartBeats Logo"
+        />
+        <a href="dashboard.html">Dashboard</a>
+        <a href="q&a.html">Q&A</a>
+        <a href="contact.html">Contact</a>
+        </div>
+        <div class="header-right">
+        <button onclick="login.html" class="second-button orange">Login</button>
+        <button class="language-button">En</button>
+        </div>
+    </div>
+
+`);
