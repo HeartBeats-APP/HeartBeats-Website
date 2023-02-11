55d33df14d412f659354a8eb05a8268431fe055c
MatthewDlr <matthieu.delarue5@gmail.com>
2023-02-11 17:51:14 +0100

    Header updated & new font

--- Header/header.js
+++ Header/header.js
@@ -2,22 +2,20 @@
 
 document.write(`
 
-    <link rel="stylesheet" href="Header/header.css"/>
-    <div class="header">
-        <div class="header-left">
-        <img
-            onclick="window.location.href='index.html'"
-            src="images/Logo.svg"
-            alt="HeartBeats Logo"
-        />
+<link rel="stylesheet" href="Header/header.css"/>
+
+<div class="header">
+    <div class="header-left">
+        <img onclick="window.location.href='index.html'" src="images/Logo.svg" alt="HeartBeats Logo" />
         <a href="dashboard.html">Dashboard</a>
         <a href="q&a.html">Q&A</a>
         <a href="contact.html">Contact</a>
-        </div>
-        <div class="header-right">
+    </div>
+    <div class="header-right">
         <button onclick="login.html" class="second-button orange">Login</button>
         <button class="language-button">En</button>
-        </div>
     </div>
+</div>
+
 
 `);
