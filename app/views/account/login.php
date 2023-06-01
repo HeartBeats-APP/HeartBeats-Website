ad888a457ee6ad3fc91d834eaea8a6bb57a34795
Ma ti yeux <57815261+MatthewDlr@users.noreply.github.com>
2023-06-01 20:09:27 +0200

    🐛 Added Goofle Auth path

--- app/views/account/login.php
+++ app/views/account/login.php
@@ -26,7 +26,7 @@
           </div>
 
           <!-- Google Sign-In -->
-          <div id="g_id_onload" data-client_id="407839619879-b18h6590qstnspu3ku9fs4nhbdhpjdds.apps.googleusercontent.com" data-context="use" data-ux_mode="popup" data-login_uri="/account/googleAuth" data-auto_select="true" data-close_on_tap_outside="false" data-itp_support="true">
+          <div id="g_id_onload" data-client_id="407839619879-b18h6590qstnspu3ku9fs4nhbdhpjdds.apps.googleusercontent.com" data-context="use" data-ux_mode="popup" data-login_uri="https://heart-beats.fr/account/googleAuth" data-auto_select="true" data-close_on_tap_outside="false" data-itp_support="true">
           </div>
           <div class="g_id_signin" data-type="standard" data-shape="rectangular" data-theme="filled_black" data-text="continue_with" data-size="large" data-logo_alignment="left">
           </div>
