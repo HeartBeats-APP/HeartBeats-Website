959a8912a1df94b4e54a942eeebd120467bfebe3
Redhoxx <red.h0x50@gmail.com>
2023-06-06 11:51:54 +0200

    test dashboard graph (2)
    changement du js et remove de la fonction précédente bien trop gourmande en performances

--- public/js/dashboard/dashboard-script.js
+++ public/js/dashboard/dashboard-script.js
@@ -7,7 +7,7 @@ let BpmLevelSet = [];
 function createChart(chartId, label, data, maxScaleValue, colorCurve) {
   var ctx = document.getElementById(chartId).getContext("2d");
   var chartData = {
-    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12 ,13 ,14 ,15],
+    labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     datasets: [
       {
         label: label,
@@ -15,8 +15,8 @@ function createChart(chartId, label, data, maxScaleValue, colorCurve) {
         backgroundColor: "rgba(" + colorCurve + ", 0.5)",
         borderColor: "rgba(" + colorCurve + ", 1)",
         borderWidth: 4,
-        fill : true,
-        fillColor : "rgba(" + colorCurve + ", 0.25)",
+        fill: true,
+        fillColor: "rgba(" + colorCurve + ", 0.25)",
       },
     ],
   };
@@ -51,86 +51,6 @@ function createChart(chartId, label, data, maxScaleValue, colorCurve) {
     options: options,
   });
 }
-
-// Appeler la fonction pour créer les graphiques au chargement de la page
-window.addEventListener("load", function () {
-  getData();
-  if (TemperatureSet.length < 15) {
-    setTimeout(function () {
-      createGraphs();
-    }, 750);
-  }
-});
-
-function createGraphs() {
-  createChart(
-    "temperature-chart",
-    "Température",
-    TemperatureSet,
-    50,
-    "255, 99, 132"
-  );
-
-  createChart("humidity-chart", "Humidité", HumiditySet, 100, "54, 162, 235");
-
-  createChart(
-    "sound-level-chart",
-    "Niveau sonore",
-    SoundLevelSet,
-    100,
-    "255, 206, 86"
-  );
-
-  createChart("bpm-level-chart", "BPM", BpmLevelSet, 200, "75, 192, 192");
-}
-
-// Sélectionnez tous les éléments graph
-const graphs = document.querySelectorAll(".graph");
-
-// Ajoutez un gestionnaire d'événement survol à chaque élément graph
-graphs.forEach((graph) => {
-  graph.addEventListener("mouseenter", () => {
-    // Ajoutez la classe "hover" à l'élément graph survolé
-    graph.classList.add("hover");
-  });
-
-  graph.addEventListener("mouseleave", () => {
-    // Supprimez la classe "hover" de l'élément graph lorsque la souris quitte le graphique
-    graph.classList.remove("hover");
-  });
-
-  // Ajoutez un gestionnaire d'événement clic à chaque élément graph
-  graph.addEventListener("click", () => {
-    // Supprimez la classe "clicked" de tous les éléments graph
-    graphs.forEach((otherGraph) => {
-      otherGraph.classList.remove("clicked");
-    });
-
-    // Ajoutez la classe "clicked" à l'élément graph cliqué
-    graph.classList.add("clicked");
-
-    // Ajoutez ou supprimez la classe "clicked" du conteneur graph-container
-    const graphContainer = document.querySelector(".graph-container");
-    graphContainer.classList.add("clicked");
-  });
-});
-
-// Ajoutez un gestionnaire d'événement clic sur le document entier
-document.addEventListener("click", (event) => {
-  // Vérifiez si l'élément cliqué est en dehors du graph-container et des graphiques agrandis
-  const isOutsideGraphContainer = !event.target.closest(".graph-container");
-  const isOutsideGraphs = !event.target.closest(".graph.clicked");
-
-  if (isOutsideGraphContainer && isOutsideGraphs) {
-    // Supprimez la classe "clicked" de tous les graphiques et du conteneur graph-container
-    graphs.forEach((graph) => {
-      graph.classList.remove("clicked");
-    });
-
-    const graphContainer = document.querySelector(".graph-container");
-    graphContainer.classList.remove("clicked");
-  }
-});
 
 async function getSensorsData() {
   var request = new XMLHttpRequest();
@@ -179,8 +99,81 @@ async function updateData() {
   BpmLevelSet.push(data.bpmLevel);
 }
 
-setInterval(updateData, 4000);
+function createGraphs() {
+  var graphContainers = document.querySelectorAll(".graph-container");
+  graphContainers.forEach(function (container) {
+    container.innerHTML = "";
+  });
 
-window.addEventListener('resize', function() {
-  location.reload();
+  createChart(
+    "temperature-chart",
+    "Température",
+    TemperatureSet,
+    50,
+    "255, 99, 132"
+  );
+
+  createChart("humidity-chart", "Humidité", HumiditySet, 100, "54, 162, 235");
+
+  createChart(
+    "sound-level-chart",
+    "Niveau sonore",
+    SoundLevelSet,
+    100,
+    "255, 206, 86"
+  );
+
+  createChart("bpm-level-chart", "BPM", BpmLevelSet, 200, "75, 192, 192");
+}
+
+window.addEventListener("load", function () {
+  getData();
+  if (TemperatureSet.length < 15) {
+    setTimeout(function () {
+      createGraphs();
+    }, 750);
+  }
 });
+
+window.addEventListener("resize", function () {
+  createGraphs();
+});
+
+const graphs = document.querySelectorAll(".graph");
+
+graphs.forEach((graph) => {
+  graph.addEventListener("mouseenter", () => {
+    graph.classList.add("hover");
+  });
+
+  graph.addEventListener("mouseleave", () => {
+    graph.classList.remove("hover");
+  });
+
+  graph.addEventListener("click", () => {
+    graphs.forEach((otherGraph) => {
+      otherGraph.classList.remove("clicked");
+    });
+
+    graph.classList.add("clicked");
+
+    const graphContainer = document.querySelector(".graph-container");
+    graphContainer.classList.add("clicked");
+  });
+});
+
+document.addEventListener("click", (event) => {
+  const isOutsideGraphContainer = !event.target.closest(".graph-container");
+  const isOutsideGraphs = !event.target.closest(".graph.clicked");
+
+  if (isOutsideGraphContainer && isOutsideGraphs) {
+    graphs.forEach((graph) => {
+      graph.classList.remove("clicked");
+    });
+
+    const graphContainer = document.querySelector(".graph-container");
+    graphContainer.classList.remove("clicked");
+  }
+});
+
+setInterval(updateData, 4000);
